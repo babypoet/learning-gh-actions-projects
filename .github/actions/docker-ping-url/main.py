d42a93288b89cfced9f00f94daad86f4b1bde7de
Prince Agyei <ybktalian@gmail.com>
2026-01-18 07:06:22 +0000

    completed python script and added additional  workflow logic

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,38 @@
+import os
+import requests 
+import time
+
+
+def ping_url(url, delay, max_trials):
+    trials  = 0
+
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable.")
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is not reachable. Retrying in {delay} seconds...")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL: {url}. Please provide a valid URL.")
+            return False  
+        
+    return False
+
+def run():
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+
+    website_reacheable = ping_url(website_url, delay, max_trials)
+    
+    if not website_reacheable:
+        raise Exception(f"Website {website_url} is not reachable")
+    print(f"Website {website_url} is reachable.")
+
 if __name__ == "__main__":
-    print("Hello Docker Ping URL Action User")
+    run()
